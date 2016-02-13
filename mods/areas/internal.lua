6a5ede5487ec0cf37d77e3557292811fc59aca0d
Wuzzy <almikes@aol.com>
2016-02-13 13:59:51 +0100

    Repair tutorial areas

--- mods/areas/internal.lua
+++ mods/areas/internal.lua
@@ -1,6 +1,6 @@
 -- Load the areas table from the save file
 function areas:load()
-	local file, err = io.open(self.config.filename, "r")
+	local file, err = io.open(minetest.get_modpath("areas") .. "/areas.dat", "r")
 	if err then
 		self.areas = self.areas or {}
 		return err
